924811936c00f4413a91e0555e8d41b5f58d086c
iganin <hironobu.iga.developer@gmail.com>
2018-09-15 23:46:55 +0900

    BlocにgetterとFunctionを追加

--- login_bloc/lib/blocs/bloc.dart
+++ login_bloc/lib/blocs/bloc.dart
@@ -3,5 +3,13 @@ import 'dart:async';
 class Bloc {
   final emailController = StreamController<String>();
   final passwordController = StreamController<String>();
+  
+  // streamへのデータ追加
+  Function(String) get changeEmail => emailController.sink.add;
+  Function(String) get changePassword => passwordController.sink.add;
+  
+  // streamからのデータ取得
+  Stream<String> get email => emailController.stream;
+  Stream<String> get password => passwordController.stream;
 }
 
