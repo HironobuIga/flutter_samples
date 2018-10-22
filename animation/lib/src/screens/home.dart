32267a380dcbc93b6405d0d2f86b297e03a6fd33
iganin <hironobu.iga.developer@gmail.com>
2018-10-22 22:42:31 +0900

    [update]animation試作用クラスの雛形作成

---
+++ animation/lib/src/screens/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  Widget build(context) {}
+}
