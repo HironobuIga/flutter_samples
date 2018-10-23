4da3ceaeab7c68942147d61dfde5cb735980a85b
iganin <hironobu.iga.developer@gmail.com>
2018-10-23 23:20:09 +0900

    [add]Scaffoldに画像を表示

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,5 +7,16 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  Widget build(context) {}
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation!"),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
+  }
 }
