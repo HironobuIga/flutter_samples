846d7adb7373d73caaf5e5f0e269e813684cb28b
iganin <hironobu.iga.developer@gmail.com>
2018-11-04 13:41:51 +0900

    タップで元に戻るアニメーションもされるよう修正

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -24,8 +24,14 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         curve: Curves.easeIn,
       ),
     );
+  }
 
-    catController.forward();
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed){
+      catController.forward();
+    }
   }
 
   Widget build(context) {
@@ -33,7 +39,10 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       appBar: AppBar(
         title: Text("Animation!"),
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        child: buildAnimation(),
+        onTap: onTap,
+      )
     );
   }
 
