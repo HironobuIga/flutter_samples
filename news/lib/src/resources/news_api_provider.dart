7156e21d3ab5916f2983a5fac41aab80cd60ea2b
iganin <hironobu.iga.developer@gmail.com>
2018-12-16 21:58:46 +0900

    APIクラスの追加

---
+++ news/lib/src/resources/news_api_provider.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' show Client;
+
+class NewsApiProvider {
+
+  Client client = Client();
+
+  fetchTopIds() {
+    client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  }
+
+  fetchItem() {
+
+  }
+
+}
