a14691b9a6ae47431de5588a5abbfe95c5c6216c
iganin <hironobu.iga.developer@gmail.com>
2018-12-23 00:23:45 +0900

    [add]テスト追加

--- news/test/src/resources/news_api_provider_test.dart
+++ news/test/src/resources/news_api_provider_test.dart
@@ -5,7 +5,14 @@ import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
 void main() {
-  test('FetchTopIds returns a list of ids', () {
+  test('FetchTopIds returns a list of ids', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
 
+
+    final ids = await newsApi.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
   });
 }
