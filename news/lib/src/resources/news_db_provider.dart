297e8768bb6d836178bb53308915a6c48fd413e6
iganin <hironobu.iga.developer@gmail.com>
2018-12-27 23:47:39 +0900

    [add]DBアクセスクラス追加

---
+++ news/lib/src/resources/news_db_provider.dart
@@ -0,0 +1,16 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+
+  init() async {
+    
+  }
+
+}
