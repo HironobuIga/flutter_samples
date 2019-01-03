3d9206bfe4a8ea24dcf2ba84e956d91bcfb35558
iganin <hironobu.iga.developer@gmail.com>
2019-01-03 20:59:51 +0900

    [add]databaseからitemを取得するメソッドの仮実装

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -38,4 +38,18 @@ class NewsDbProvider {
     );
   }
 
+  fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      
+    }
+
+    return null;
+  }
 }
