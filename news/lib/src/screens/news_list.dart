afbb99683fae39cbd0123c4d55dbe52d9617ecb6
iganin <hironobu.iga.developer@gmail.com>
2019-01-14 01:02:49 +0900

    [modify]iOS Simulatorでのアプリ実行 [add]リスト表示追加

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -6,7 +7,30 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news'),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? Text('Im visible $index')
+                : Text('You cant see me $index');
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+          () => 'hi',
     );
   }
 }
