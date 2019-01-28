3deaef356d032620fb399741df71bf696f834648
iganin <hironobu.iga.developer@gmail.com>
2019-01-28 21:38:04 +0900

    [add]BlocにScanStreamTransformerを追加

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repsitory.dart';
@@ -11,6 +12,17 @@ class StoriesBloc {
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
+  }
+
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        
+      },
+      <int, Future<ItemModel>> {
+
+      },
+    );
   }
 
   dispose() {
