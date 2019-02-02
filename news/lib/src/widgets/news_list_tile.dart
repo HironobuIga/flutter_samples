25e11c10c898e2d36b3f58e0e7ac4959fd83fd33
iganin <hironobu.iga.developer@gmail.com>
2019-02-02 14:50:16 +0900

    [update]tileの内部実装

---
+++ news/lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        }
+
+        
+      },
+    );
+  }
+}
