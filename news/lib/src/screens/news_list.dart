d1ece8589eb1d1293ce002970e377c5fc8e2e545
iganin <hironobu.iga.developer@gmail.com>
2019-02-03 16:02:00 +0900

    [add]ListViewに作成したTileを表示するよう修正

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 import '../blocs/stories_provider.dart';
+import '../widgets/news_list_tile.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -28,7 +28,12 @@ class NewsList extends StatelessWidget {
         return ListView.builder(
           itemCount: snapshot.data.length,
           itemBuilder: (context, int index) {
-            return Text('${snapshot.data[index]}');
+            bloc.fetchItem(snapshot.data[index]);
+
+            return NewsListTile(
+              itemId: snapshot.data[index],
+            );
+
           },
         );
       },
