28ad75f0caca414aca71ebc3186917651cc546c6
iganin <hironobu.iga.developer@gmail.com>
2019-03-01 23:59:16 +0900

    [add]リポジトリクラスとDAOクラスにキャッシュクリアメソッドを実装

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -68,6 +68,10 @@ class NewsDbProvider implements Source, Cache {
       conflictAlgorithm: ConflictAlgorithm.ignore,
     );
   }
+
+  Future<int> clear() {
+    return db.delete("Items");
+  }
 }
 
 final newsDbProvider = NewsDbProvider();
--- news/lib/src/resources/repsitory.dart
+++ news/lib/src/resources/repsitory.dart
@@ -35,6 +35,12 @@ class Repository {
 
     return item;
   }
+
+  clearCache() async {
+    for (var cache in caches) {
+      await cache.clear();
+    }
+  }
 }
 
 abstract class Source {
@@ -44,4 +50,5 @@ abstract class Source {
 
 abstract class Cache {
   Future<int> addItem(ItemModel item);
+  Future<int> clear();
 }
