124f53133d997b991c199884b011c43589e466b2
iganin <hironobu.iga.developer@gmail.com>
2019-03-01 23:41:10 +0900

    [add]リスト画面にリフレッシュWidgetを追加

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../blocs/stories_provider.dart';
 import '../widgets/news_list_tile.dart';
+import '../widgets/refresh.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -25,16 +26,18 @@ class NewsList extends StatelessWidget {
           );
         }
 
-        return ListView.builder(
-          itemCount: snapshot.data.length,
-          itemBuilder: (context, int index) {
-            bloc.fetchItem(snapshot.data[index]);
+        return Refresh(
+          child: ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, int index) {
+              bloc.fetchItem(snapshot.data[index]);
 
-            return NewsListTile(
-              itemId: snapshot.data[index],
-            );
+              return NewsListTile(
+                itemId: snapshot.data[index],
+              );
 
-          },
+            },
+          ),
         );
       },
     );
---
+++ news/lib/src/widgets/refresh.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Refresh extends StatelessWidget {
+  final Widget child;
+
+  Refresh({this.child});
+
+  Widget build(context) {
+    return RefreshIndicator(
+      child: child,
+      onRefresh: () {
+
+      },
+    );
+  }
+}
