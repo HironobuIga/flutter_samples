9cf74f068f3cbb349effaed901842dfe2ea9bdef
iganin <hironobu.iga.developer@gmail.com>
2019-03-10 15:40:58 +0900

    [modify]詳細画面をScaffoldに変更

--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 class NewsDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Detail');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+      ),
+      body: Text('Detail'),
+    );
   }
 }
