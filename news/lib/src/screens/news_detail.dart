2871ea76e0ac3572ced16c68119c8929c4724fc4
iganin <hironobu.iga.developer@gmail.com>
2019-03-11 23:44:23 +0900

    [modify]詳細画面にIDを渡すよう修正

--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  NewsDetail({this.itemId});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('Detail'),
+      body: Text('${itemId}'),
     );
   }
 }
