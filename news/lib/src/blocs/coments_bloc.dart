56d329a93cf95d1cedefb0ac3ec13d289ec27b28
iganin <hironobu.iga.developer@gmail.com>
2019-03-13 00:04:42 +0900

    詳細画面用bloc実装

--- news/lib/src/blocs/coments_bloc.dart
+++ news/lib/src/blocs/coments_bloc.dart
@@ -1,5 +1,19 @@
-
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repsitory.dart';
 
 class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
 
+  Observable<Map<int, Future<ItemModel>>> get itemWithComment =>
+  _commentsOutput.stream;
+
+  Function(int) get fetchItemWithcomments => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
 }
