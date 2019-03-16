ec8ce962373c4fba800c3c3858776e5b39091498
iganin <hironobu.iga.developer@gmail.com>
2019-03-16 23:09:00 +0900

    [add]BlocにTransformer実装

--- news/lib/src/blocs/coments_bloc.dart
+++ news/lib/src/blocs/coments_bloc.dart
@@ -4,6 +4,7 @@ import '../models/item_model.dart';
 import '../resources/repsitory.dart';
 
 class CommentsBloc {
+  final _repository = Repository();
   final _commentsFetcher = PublishSubject<int>();
   final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
 
@@ -11,6 +12,22 @@ class CommentsBloc {
   _commentsOutput.stream;
 
   Function(int) get fetchItemWithcomments => _commentsFetcher.sink.add;
+
+  CommentsBloc() {
+    _commentsFetcher.stream.transform(_commentsTransformer()).pipe(_commentsOutput);
+  }
+
+  _commentsTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+            (cache, int id, index) {
+              cache[id] = _repository.fetchItem(id);
+              cache[id].then((ItemModel item) {
+                item.kids.forEach((kidId) => fetchItemWithcomments(kidId));
+              });
+        },
+        <int, Future<ItemModel>> { }
+    );
+  }
 
   dispose() {
     _commentsFetcher.close();
