c2764c3071110ca573d07d16ebdbfabe13378427
iganin <hironobu.iga.developer@gmail.com>
2019-03-17 14:08:36 +0900

    [modify]タイトルが中央にくるよう修正

--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -46,6 +46,7 @@ class NewsDetail extends StatelessWidget {
   Widget buildTitle(ItemModel item) {
     return Container(
       margin: EdgeInsets.all(10.0),
+      alignment: Alignment.topCenter,
       child: Text(
         item.title,
         textAlign: TextAlign.center,
